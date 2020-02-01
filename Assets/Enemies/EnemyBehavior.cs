eb9cd0e041e30385333c81d852fd77c62384ca93
Jesse Thompson <jesseryanthompson@gmail.com>
2020-02-01 11:03:06 -0600

    WIP enemy spawner

---
+++ Assets/Enemies/EnemyBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject target;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.target)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Enemies/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public int spawnInterval = 300;
+    public GameObject[] targets;
+    public GameObject[] enemyTypes;
+
+    private int tick = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        tick += 1;
+
+        if (tick > spawnInterval)
+        {
+            tick -= spawnInterval;
+
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        var enemyIndex = Random.Range(0, enemyTypes.Length - 1);
+        GameObject go = GameObject.Instantiate(enemyTypes[enemyIndex], new Vector3(Random.Range(0,5),0, Random.Range(0, 5)), Quaternion.identity);
+
+        var targetIndex = Random.Range(0, targets.Length - 1);
+
+        go.GetComponent<EnemyBehavior>().SetTarget(targets[targetIndex]);
+    }
+}
