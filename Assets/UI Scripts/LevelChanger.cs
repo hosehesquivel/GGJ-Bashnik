c1bb868cc1cbecb141c745b34b326750ee0df751
hosehesquivel <49042246+hosehesquivel@users.noreply.github.com>
2020-02-02 03:55:18 -0600

    Main Menu update

---
+++ Assets/UI Scripts/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+	public Animator animator;
+ 
+	public float transitionTime = 30f;
+ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // need to press play button.
+		{
+			LoadNextLevel();
+		}
+    }
+	
+	public void LoadNextLevel()
+	{
+		StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+	}
+	
+	IEnumerator LoadLevel(int levelIndex)
+	{
+		animator.SetTrigger("Start");
+		
+		yield return new WaitForSeconds(transitionTime);
+		
+		SceneManager.LoadScene(levelIndex);
+	}
+}
